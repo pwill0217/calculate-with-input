f5f352139408ac016b6e70ff69e9726efe911128
pwill0217 <73377167+pwill0217@users.noreply.github.com>
2024-02-23 00:08:18 -0500

    Create calcinpu.py

---
+++ calcinpu.py
@@ -0,0 +1,9 @@
+#Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn. 
+# Sample value of n is 5 
+# Expected Result : 615
+
+n = int(input("Enter your value: "))
+nn = n*11
+nnn = n*111
+print(n+nn+nnn)
+
